5e551e402fe3695db103c44fe58600f16a0bfabc
allowmahdi <allowmahdi@gmail.com>
2018-05-20 06:01:49 +0430

    Initial commit

---
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -0,0 +1,29 @@
+/*
+ * This is the source code of Telegram for Android v. 3.x.x.
+ * It is licensed under GNU GPL v. 2 or later.
+ * You should have received a copy of the license in this archive (see LICENSE).
+ *
+ * Copyright Nikolai Kudashov, 2013-2017.
+ */
+
+package org.telegram.messenger;
+
+public class BuildVars
+{
+	public static boolean DEBUG_VERSION                = false;
+	public static boolean DEBUG_PRIVATE_VERSION        = false;
+	public static int     BUILD_VERSION                = 927;
+	public static String  BUILD_VERSION_STRING         = "3.18";
+	public static String  BUILD_PACKAGENAME            = "ir.topgeram.messenger";
+	public static int     APP_ID                       = 85257; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
+	public static String  APP_HASH                     = "aada6d74ff8aec9e6fb3544a7ade3229"; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
+	public static String  HOCKEY_APP_HASH              = "aada6d74ff8aec9e6fb3544a7ade3229";
+	public static String  HOCKEY_APP_HASH_DEBUG        = "aada6d74ff8aec9e6fb3544a7ade3229";
+	public static String  HOCKEY_APP_HASH_DEBUG_SECRET = "aada6d74ff8aec9e6fb3544a7ade3229";
+	public static String  GCM_SENDER_ID                = "584062367042";
+	public static String  SEND_LOGS_EMAIL              = "pinfo@gmail.com";
+	public static String  BING_SEARCH_KEY              = ""; //obtain your own KEY at https://www.bing.com/dev/en-us/dev-center
+	public static String  FOURSQUARE_API_KEY           = ""; //obtain your own KEY at https://developer.foursquare.com/
+	public static String  FOURSQUARE_API_ID            = ""; //obtain your own API_ID at https://developer.foursquare.com/
+	public static String  FOURSQUARE_API_VERSION       = "20153326";
+}
